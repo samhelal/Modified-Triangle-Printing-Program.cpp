66de7d56e44ac4adcee6f6a5c15e541dd54c815e
samhelal <93845103+samhelal@users.noreply.github.com>
2022-05-03 05:23:23 +0200

    Add files via upload

--- Modified-Triangle-Printing-Program.cpp
+++ Modified-Triangle-Printing-Program.cpp
@@ -8,16 +8,21 @@ int main (){
 
     for( int x(1) ; x<=10 ; x++ ){
         cout << " " << endl ;
+
         for(int y(1) ; y<=x ; y++ ){
             cout << "*"  ;
         }
+
         for(int z(10) ; z>x ; z--){
             cout << " " ;
         }
+
         cout <<" " ;
+        
         for(int a(10) ; a>=x ; a--){
             cout << "*" ;
         }
+
         for(int b(1) ; b<x ; b++ ){
             cout << " " ;
         }
@@ -25,15 +30,19 @@ int main (){
         for(int c(1) ; c<x ; c++){
             cout << " " ;
         }
+        
         cout << " " ;
 
         for(int d(10) ; d>=x ; d--){
             cout << "*" ;
         }
+        
         cout <<" " ;
+        
         for(int e(10) ; e>x ; e--){
             cout << " " ;
         }
+        
         for(int f(1) ; f<=x ; f++){
             cout << "*" ;
         }
